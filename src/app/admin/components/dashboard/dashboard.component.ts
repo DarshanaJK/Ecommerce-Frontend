86fb3b0b36eddcb79f9e585e9d3938081aa1d4ce
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-23 00:38:04 +0530

    Create Showing Products To Admin

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AdminService} from "../../service/admin.service";
+import {elementAt} from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  products: any[] = [];
+
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.products = [];
+    this.adminService.getAllProducts().subscribe(res => {
+      res.forEach(element => {
+        element.processImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      })
+    })
+  }
 }
